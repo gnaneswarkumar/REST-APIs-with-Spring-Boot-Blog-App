c3ffdaa9ea5eb39676d3dd24f454ee841a3765c6
GK <gnaneswarkumar.k@gmail.com>
2023-09-04 22:26:42 +0530

    implemented post service

--- src/main/java/com/springboot/blog/springbootblogrestapi/SpringBootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/SpringBootBlogRestApiApplication.java
@@ -1,7 +1,9 @@
 package com.springboot.blog.springbootblogrestapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +11,11 @@ import java.util.Set;
 
 @SpringBootApplication
 public class SpringBootBlogRestApiApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBlogRestApiApplication.class, args);
 	}
--- src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/service/impl/PostServiceImpl.java
@@ -7,6 +7,7 @@ import com.springboot.blog.springbootblogrestapi.payload.PostResponse;
 import com.springboot.blog.springbootblogrestapi.repository.PostRepository;
 import com.springboot.blog.springbootblogrestapi.service.PostCommentCmnService;
 import com.springboot.blog.springbootblogrestapi.service.PostService;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -21,10 +22,16 @@ public class PostServiceImpl implements PostService {
     private PostRepository postRepository;
     private PostCommentCmnService postCommentCmnService;
 
-    public PostServiceImpl(PostRepository postRepository, PostCommentCmnService postCommentCmnService) {
+    private ModelMapper mapper;
+    public PostServiceImpl(
+            PostRepository postRepository,
+            PostCommentCmnService postCommentCmnService,
+            ModelMapper mapper
+    ) {
 
         this.postRepository = postRepository;
         this.postCommentCmnService = postCommentCmnService;
+        this.mapper = mapper;
     }
 
     @Override
@@ -90,21 +97,21 @@ public class PostServiceImpl implements PostService {
 
     //Convert Entity into DTO
     private PostDto mapToDTO(Post post){
-        PostDto postDto = new PostDto();
-        postDto.setId(post.getId());
-        postDto.setTitle(post.getTitle());
-        postDto.setDescription(post.getDescription());
-        postDto.setContent(post.getContent());
-
+        PostDto postDto = mapper.map(post, PostDto.class);
+//        PostDto postDto = new PostDto();
+//        postDto.setId(post.getId());
+//        postDto.setTitle(post.getTitle());
+//        postDto.setDescription(post.getDescription());
+//        postDto.setContent(post.getContent());
         return postDto;
     }
 
     private Post mapToEntity(PostDto postDto){
-        Post post = new Post();
-        post.setTitle(postDto.getTitle());
-        post.setDescription(postDto.getDescription());
-        post.setContent(postDto.getContent());
-
+        Post post = mapper.map(postDto, Post.class);
+//        Post post = new Post();
+//        post.setTitle(postDto.getTitle());
+//        post.setDescription(postDto.getDescription());
+//        post.setContent(postDto.getContent());
         return post;
     }
 }
